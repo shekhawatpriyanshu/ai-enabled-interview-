3dda3e13f553547cbba0e09e42c2fd23f4c55751
shekhawatpriyanshu <shekhawtpriyanshu@gmail.com>
2026-07-29 12:41:54 +0530

    add some

--- ai-enabled-interview--main/client/src/api/config.js
+++ ai-enabled-interview--main/client/src/api/config.js
@@ -1,8 +1,10 @@
-export const getBackendUrl = () => {
-  if (import.meta.env.VITE_API_URL) return import.meta.env.VITE_API_URL;
-  return typeof window !== "undefined" &&
-    (window.location.hostname === "localhost" ||
-      window.location.hostname === "127.0.0.1")
-    ? "http://localhost:3000"
-    : "https://ai-enabled-interview.onrender.com";
+const getBackendUrl = () => {
+  if (
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+  ) {
+    return "http://localhost:3000";
+  }
+
+  return "http://52.62.40.253:3000";
 };
